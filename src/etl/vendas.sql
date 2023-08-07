34c23dedce23501b0f4be2280c6999e0574e947e
Nicolas Faleiros <nicolas_gonzaga@hotmail.com>
2023-08-07 19:42:50 -0300

    Variáveis vendas

---
+++ src/etl/vendas.sql
@@ -0,0 +1,120 @@
+WITH tb_pedido_item As(
+    SELECT 
+        t2.*,
+        t1.dtPedido
+
+    FROM pedido AS t1
+
+    LEFT JOIN item_pedido AS t2
+    ON t1.idPedido = t2.idPedido
+
+    WHERE t1.dtPedido < '2018-01-01'
+    AND t1.dtPedido >= DATE('2018-01-01','-6 MONTH')
+    AND t2.idVendedor IS NOT NULL
+),
+
+tb_summary AS (
+    SELECT
+        idVendedor,
+        COUNT(DISTINCT idPedido) AS qtdePedidos,
+        COUNT(DISTINCT DATE(dtPedido)) AS qtdeDias,
+        COUNT(idProduto) AS qtdeItens,
+        MIN(julianday('2018-01-01') - julianday(dtPedido)) AS qtdeRecencia,
+        SUM(vlPreco) / COUNT(DISTINCT idPedido) AS avgTicket,
+        AVG(vlPreco) AS avgValorProduto,
+        MAX(vlPreco) AS maxValorProduto,
+        MIN(vlPreco) AS minValorProduto,
+        COUNT(idProduto) / COUNT(DISTINCT idPedido) AS avgProdutoPedido
+
+    FROM tb_pedido_item
+
+    GROUP BY idVendedor
+),
+
+tb_pedido_summary AS (
+    SELECT
+        idVendedor,
+        idPedido,
+        SUM(vlPreco) AS vlPreco
+
+    FROM tb_pedido_item
+
+    GROUP BY idVendedor, idPedido
+),
+
+tb_min_max AS (
+    SELECT 
+        idVendedor,
+        AVG(vlPreco) AS avgVlPedido,
+        MIN(vlPreco) AS minVlPedido,
+        MAX(vlPreco) AS maxVlPedido
+
+    FROM tb_pedido_summary
+
+    GROUP BY idVendedor
+),
+
+tb_life AS (
+    SELECT 
+        t2.idVendedor,
+        SUM(vlPreco) AS LTV,
+        MAX(julianday('2018-01-01') - julianday(DATE(dtPedido))) AS qtdeDiasBase
+
+    FROM pedido AS t1
+
+    LEFT JOIN item_pedido AS t2
+    ON t1.idPedido = t2.idPedido
+
+    WHERE t1.dtPedido < '2018-01-01'
+    AND t2.idVendedor IS NOT NULL
+
+    GROUP BY 1
+),
+
+tb_dtpedido AS (
+    SELECT
+        idVendedor,
+        DATE(dtPedido) AS dtPedido
+
+    FROM tb_pedido_item
+
+    ORDER BY 1,2
+),
+
+tb_lag AS (
+    SELECT
+        *,
+        LAG(dtPedido) OVER (PARTITION BY idVendedor ORDER BY dtPedido) AS lag1
+
+    FROM tb_dtpedido
+),
+
+tb_intervalo AS (
+    SELECT
+        idVendedor,
+        AVG(julianday(DATE(dtPedido)) - julianday(DATE(lag1))) AS avgIntervaloVendas
+
+    FROM tb_lag
+
+    GROUP BY idVendedor
+)
+
+SELECT
+    '2018-01-01' AS dtReference,
+    t1.*,
+    t2.minVlPedido,
+    t2.maxVlPedido,
+    t3.LTV,
+    t3.qtdeDiasBase,
+    t4.avgIntervaloVendas
+
+FROM tb_summary AS t1
+
+LEFT JOIN tb_min_max AS t2
+ON t1.idVendedor = t2.idVendedor
+
+LEFT JOIN tb_life AS t3
+ON t1.idVendedor = t3.idVendedor
+
+LEFT JOIN tb_intervalo AS t4
+ON t1.idVendedor = t4.idVendedor;
